f0705ae7d88d006f489bb0ded1ed528cd8891576
DESKTOP-URF28CR\mnc5 <mnc5@naver.com>
2021-06-11 18:50:47 +0900

    2021.06.11
    최대부분곱 추가

---
+++ partial_product_max/test.java
@@ -0,0 +1,12 @@
+package partial_product_max;
+
+public class test {
+
+	public static void main(String[] args) {
+		PartialProductMax ppm = new PartialProductMax();
+		int arr[] = {-6, 12, -7, 0, 14, -7, 5};
+		System.out.println("최대곱: " + ppm.PPM(arr));
+		
+	}
+
+}
