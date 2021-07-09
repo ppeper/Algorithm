159236ce80a5e1474cb85b1579e106f677b5d4d8
Joonhoo <mnc5@naver.com>
2021-07-09 22:11:30 +0900

    2021.07.09

--- Programmers/Camouflage.java
+++ Programmers/Camouflage.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 // 하의	청바지
 // 겉옷	긴 코트
 // 스파이가 가진 의상들이 담긴 2차원 배열 clothes가 주어질 때 서로 다른 옷의 조합의 수를 return 하도록 solution 함수를 작성해주세요.
-class Solution {
+class Camouflage {
     public int solution(String[][] clothes) {
         int answer = 0, conbine = 1;
         HashMap<String, Integer> spyItem = new HashMap<>();
