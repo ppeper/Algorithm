666e598e7391f00cc01be8137d210e19ff2029c1
Joonhoo <mnc5@naver.com>
2021-08-16 23:52:54 +0900

    2021.08.16

--- Programmers/Weekly_Challenge/Week_1.java
+++ Programmers/Weekly_Challenge/Week_1.java
@@ -6,7 +6,7 @@ package Programmers.Weekly_Challenge;
 // 놀이기구를 count번 타게 되면 현재 자신이 가지고 있는 금액에서 얼마가 모자라는지를 return 하도록 solution 함수를 완성하세요.
 // 단, 금액이 부족하지 않으면 0을 return 하세요.
 
-class Solution {
+class Week_1{
     public long solution(int price, int money, int count) {
         long answer = 0;
         long total = 0;
--- Programmers/Weekly_Challenge/Week_2.java
+++ Programmers/Weekly_Challenge/Week_2.java
@@ -34,7 +34,7 @@ package Programmers.Weekly_Challenge;
 //50점 미만	F
 //학생들의 점수가 담긴 정수형 2차원 배열 scores가 매개변수로 주어집니다. 이때, 학생들의 학점을 구하여 하나의 문자열로 만들어서 return 하도록 solution 함수를 완성해주세요.
 
-class Solution {
+class Week_2 {
     public String solution(int[][] scores) {
         StringBuilder answer = new StringBuilder();
         for (int i = 0; i < scores.length; i++) {
