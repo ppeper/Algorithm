f2d5debdae4d742399b243781ded6447547156ed
joonhoo <mnc5@naver.com>
2023-03-02 17:17:29 +0900

    package 수정

--- BAEKJOON/p17281.java
+++ BAEKJOON/p17281.java
@@ -3,7 +3,6 @@ package BAEKJOON;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class p17281 {
